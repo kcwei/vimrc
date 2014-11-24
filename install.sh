3d6088f3849eaef0546da906c8348d6c9c4ca6f7
kcwei <wei2929@gmail.com>
2014-11-25 01:22:33 +0800

    Modify install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,6 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Install plugins
 vim +PluginInstall +qall
 
-# YCM install and setting
+# YCM install(with c family support) and setting
 ~/.vim/bundle/YouCompleteMe/install.sh --clang-completer
 ln -f -s ~/.vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
